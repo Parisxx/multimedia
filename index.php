802a193e17e0d1241245d7101fe040162ec32a4e
= <=>
2022-03-31 11:09:09 +0200

    ASDFGH

--- index.php
+++ index.php
@@ -22,40 +22,44 @@ include("functions.php");
 
     <a href="add.php" class="button"> Add </a>
 
-
+    <div class="wrap">
     <div class="content1">
         <p class="text"> Songs </p>
-        <?php
-           $sql = "SELECT * FROM watchlist WHERE media_type= '2'";
-           $result = $conn->query($sql);
-           while($row = $result->fetch(PDO::FETCH_ASSOC)){
-            $watched = $row['watched'];
+        
+            <?php
+            $sql = "SELECT * FROM watchlist WHERE media_type= '2'";
+            $result = $conn->query($sql);
+            while($row = $result->fetch(PDO::FETCH_ASSOC)){
+                $watched = $row['watched'];
 
-        echo "<div class='carts'>";
-        echo "<form method='post'>";
-        echo "<button formaction='edit.php' class='button3' name='edit' type='button'>Edit</button>";
-        echo "<button formaction='delete.php' class='button3' name='delete' type='button'>Delete</button>";
-        echo "</form>";
+            echo "<li>";
+            echo "<div class='carts'>";
+            echo "<form method='post'>";
+            echo "<button formaction='edit.php' class='button3' name='edit' type='button'>Edit</button>";
+            echo "<button formaction='delete.php' class='button3' name='delete' type='button'>Delete</button>";
+            echo "</form>";
 
-        echo "<h1>" . $row['title'] . " - " . $row['maker'] . " </h1>";
-        echo "<h3>" . $row['platform'] . "</h3>";
-        echo "<p>" . $row['review'] . "</p>";
-        echo "<a target='_blank' href='" . $row['link'] ."'>";
-        echo "<img src='photos/" . $row['photo']."' height='200px' width='200px'>";
-        echo "</a>";
-        echo "<p> Listened: &nbsp";   
+            echo "<h1>" . $row['title'] . " - " . $row['maker'] . " </h1>";
+            echo "<h3>" . $row['platform'] . "</h3>";
+            echo "<p>" . $row['review'] . "</p>";
+            echo "<a target='_blank' href='" . $row['link'] ."'>";
+            echo "<img src='photos/" . $row['photo']."' height='200px' width='200px'>";
+            echo "</a>";
+            echo "<p> Listened: &nbsp";   
+            
 
-        if ($watched == "0"){
-            echo"<td>Yes</td>";
-        } else {
-            echo "<td>No</td>";
-        }
-  
-        echo "</div>";
-        }
-        ?>
+            if ($watched == "0"){
+                echo"<td>Yes</td>";
+            } else {
+                echo "<td>No</td>";
+            }
+    
+            echo "</div>";
+            echo "</li>";
+            }
+            ?>
+        </div>
     </div>
-
 
     <div class="content2">
         <p class="text"> Films </p>
